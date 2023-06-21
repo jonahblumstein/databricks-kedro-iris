250abaa57381a77f53cfd4528a8bcb360cdd0a9e
Jonah Blumstein <jonah_blumstein@mckinsey.com>
2023-06-20 20:07:09 -0400

    more info and find req.txt

--- onstart.sh
+++ onstart.sh
@@ -3,4 +3,4 @@ current_loc=$(pwd)
 objs_here=$(ls)
 echo "The current working directory : $current_loc"
 echo "Objects here : $objs_here"
-pip install -r Workspace/Repos/jonah_blumstein@mckinsey.com/databricks-kedro-iris/src/requirements.txt
+pip install -r /Workspace/Repos/jonah_blumstein@mckinsey.com/databricks-kedro-iris/src/requirements.txt
